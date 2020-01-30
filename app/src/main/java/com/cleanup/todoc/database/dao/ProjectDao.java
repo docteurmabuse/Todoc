9b79f60b1edd4e32cbe257b4bb24ce99ca42e1c6
docteurmabuse <laurent.tizzone@gmail.com>
2020-01-30 18:06:57 +0100

    add Dao to database dao

---
+++ app/src/main/java/com/cleanup/todoc/database/dao/ProjectDao.java
@@ -0,0 +1,8 @@
+package com.cleanup.todoc.database.dao;
+
+import android.arch.persistence.room.Dao;
+
+@Dao
+public interface ProjectDao {
+
+}
---
+++ app/src/main/java/com/cleanup/todoc/database/dao/TaskDao.java
@@ -0,0 +1,7 @@
+package com.cleanup.todoc.database.dao;
+
+import android.arch.persistence.room.Dao;
+
+@Dao
+public interface TaskDao {
+}
